fe765f9c7d4c0ac6473ac56f5068cded7a0c8ee1
autumn crossan <the@autumn.zone>
2018-02-21 18:38:39 -0500

    ASCII KNUCKTATS

--- index.rb
+++ index.rb
@@ -1,6 +1,14 @@
-puts File.read('/usr/share/dict/words')
+word = File.read('/usr/share/dict/words')
   .split("\n")
   .select { |word| word.size == 8 }
-  .map { |word| word.upcase.split('').join(' ') }
+  .map { |word| word.upcase }
   .sample
+
+puts "
+┌───┬───┬───┬───┐  ┌───┬───┬───┬───┐
+│   │   │   │   │  │   │   │   │   │
+│ %s │ %s │ %s │ %s │  │ %s │ %s │ %s │ %s │
+├___┼___┼___┼___┤  ├___┼___┼___┼___┤
+└───┴───┴───┴───┘  └───┴───┴───┴───┘
+" % [ word[0], word[1], word[2], word[3], word[4], word[5], word[6], word[7] ]
 
